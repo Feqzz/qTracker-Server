f5f9a3feeef2286aa7a504b4f049933118a89638
tarald <taraldv@gmail.com>
2020-02-29 22:18:17 +0100

    initial

---
+++ server.cpp
@@ -0,0 +1,193 @@
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <iostream>
+using namespace std;
+
+string email;
+string key;
+int code;
+
+string createMailBody()
+{
+	string body = "\"<html><body>"+key+"</body></html>\"";
+	return body;
+}
+
+void sendEmail()
+{
+	string sender = "noreply@tarves.no";
+	string subject = "\"testing cpp\"";
+	string content = "\"Content-Type: text/html\"";
+	string shellCommand = "echo "+createMailBody()+
+		 " > /tmp/mailbody && mail -a "+content+
+		 " -s "+subject+" -r "+sender+" "+email+
+		 " < /tmp/mailbody";
+	cout << shellCommand << endl;
+	system(shellCommand.c_str());
+
+}
+
+void parseBuffer(char* buffer,int length)
+{
+  code = (int)buffer[0];
+	string output = "";
+  bool swap = false;
+  int count = 0;
+	for(int i=1;i<length;i++)
+	{
+		char temp = buffer[i];
+    if(temp=='\n')
+    {
+      count++;
+      continue;
+    }
+    if(count==0)
+		{
+			email+=temp;
+		}
+    if(count==1)
+    {
+      key+=temp;
+    }
+    
+	}
+
+}
+
+int createSocket(int port)
+{
+    int s;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) 
+    {
+       perror("Unable to create socket");
+       exit(EXIT_FAILURE);
+   }
+
+   if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) 
+   {
+       perror("Unable to bind");
+       exit(EXIT_FAILURE);
+   }
+
+   if (listen(s, 1) < 0) 
+   {
+       perror("Unable to listen");
+       exit(EXIT_FAILURE);
+   }
+
+   return s;
+}
+
+void initOpenSSL()
+{ 
+    SSL_load_error_strings();	
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_SSL()
+{
+    EVP_cleanup();
+}
+
+SSL_CTX *createContext()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) 
+    {
+       perror("Unable to create SSL context");
+       ERR_print_errors_fp(stderr);
+       exit(EXIT_FAILURE);
+   }
+
+   return ctx;
+}
+
+void configureContect(SSL_CTX *ctx)
+{
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    /* Set the key and cert */
+    
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) 
+    {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) 
+    {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int sock;
+    SSL_CTX *ctx;
+
+    initOpenSSL();
+    ctx = createContext();
+
+    configureContect(ctx);
+
+    sock = createSocket(12321);
+
+    /* Handle connections */
+    while(1) {
+        struct sockaddr_in addr;
+        uint len = sizeof(addr);
+        SSL *ssl;
+        const char reply[] = "1";
+	      char readBuffer[255];
+
+        int client = accept(sock, (struct sockaddr*)&addr, &len);
+        if (client < 0) 
+        {
+            perror("Unable to accept");
+            exit(EXIT_FAILURE);
+        }
+
+        ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client);
+
+        if (SSL_accept(ssl) <= 0)
+        {
+            ERR_print_errors_fp(stderr);
+        }
+        else
+        {
+      	    SSL_read(ssl,readBuffer,255);
+      	    parseBuffer(readBuffer,255);
+      	    sendEmail();
+            //SSL_write(ssl, reply, strlen(reply));
+        }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(client);
+    }
+
+    close(sock);
+    SSL_CTX_free(ctx);
+    cleanup_SSL();
+}
