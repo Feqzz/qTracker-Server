1f2be340da8405adf9af475e1cd2e8f730d6a6d2
Tarald <taraldv@gmail.com>
2020-08-13 09:27:49 +0200

    added <br>

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
  *
  * \section intro_sec Introduction
  *
- * This document describes all the C++ classes used in our QTracker-Server.
+ * This document describes all the C++ classes used in our QTracker-Server.<br>
  * There are no ads on this page, but due to the unforunate name of the software,
  * adblockers will disable some functionality. Disable it to view the webpage as
  * intended.
